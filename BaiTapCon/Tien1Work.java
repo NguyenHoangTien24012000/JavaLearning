6228bc6c31060f51f44d8c6b16b858e425411f59
nguyenhoangtien <hoangtienflytom@gmail.com>
2023-07-06 15:30:13 +0700

    master work

--- BaiTapCon/Tien1Work.java
+++ BaiTapCon/Tien1Work.java
@@ -3,5 +3,6 @@ package BaiTapCon;
 public class Tien1Work {
     public static void main(String[] args) {
         System.out.println("tien1 lam viec");
+        System.out.println("Master work");
     }
 }
